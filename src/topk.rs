7c76a56b5f68719e214f45c50a690f2ac0c02d5c
George Ogden <george-ogden@outlook.com>
2026-05-29 19:42:40 +0100

    Use key function for topk

--- src/topk.rs
+++ src/topk.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 
 pub trait Topk: Iterator + Sized
 where
@@ -8,7 +8,7 @@ where
     #[must_use]
     /// Select the k largest items from an iterator.
     /// If there insufficient items, all are returned.
-    /// The returned items are sorted from largest to smallest.
+    /// The returned items are sorted from smallest to largest.
     fn topk(self, k: usize) -> impl IntoIterator<Item = Self::Item> {
         let mut best = BTreeSet::new();
         for (id, item) in self.enumerate() {
@@ -16,7 +16,7 @@ where
             if best.len() < k {
                 let result = best.insert((item, id));
                 debug_assert!(result, "unique item already in map");
-            } else if best.first().is_some_and(|worst| item >= worst.0) {
+            } else if best.first().is_some_and(|worst| item > worst.0) {
                 let result = best.pop_first();
                 debug_assert!(result.is_some(), "item should be in map");
                 let result = best.insert((item, id));
@@ -33,6 +33,36 @@ where
     T::Item: Ord,
 {
 }
+
+pub trait TopkBy: Iterator + Sized {
+    #[inline]
+    fn topk_by<K: Ord, F: FnMut(&Self::Item) -> K>(
+        self,
+        k: usize,
+        mut f: F,
+    ) -> impl IntoIterator<Item = Self::Item> {
+        let mut best = BTreeMap::new();
+        for (id, value) in self.enumerate() {
+            let key = f(&value);
+            #[expect(clippy::else_if_without_else)]
+            if best.len() < k {
+                let result = best.insert((key, id), value);
+                debug_assert!(result.is_none(), "unique item already in map");
+            } else if best
+                .first_key_value()
+                .is_some_and(|(worst, _)| key > worst.0)
+            {
+                let result = best.pop_first();
+                debug_assert!(result.is_some(), "item should be in map");
+                let result = best.insert((key, id), value);
+                debug_assert!(result.is_none(), "unique item already in map");
+            }
+        }
+        best.into_values()
+    }
+}
+
+impl<T> TopkBy for T where T: Iterator {}
 
 #[cfg(test)]
 #[path = "topk_test.rs"]
--- src/topk_test.rs
+++ src/topk_test.rs
@@ -1,3 +1,5 @@
+use std::{cmp::Reverse, sync::Arc};
+
 use super::*;
 use itertools::Itertools;
 use pretty_assertions::assert_eq;
@@ -62,4 +64,118 @@ fn test_topk_take_some_shuffled(seed: u64) {
         x.into_iter().topk(3).into_iter().collect_vec(),
         vec![9, 9, 10]
     );
+}
+
+#[test]
+fn test_topk_pointers() {
+    let chosen = Arc::new(0);
+    let x = vec![Arc::clone(&chosen), Arc::new(0)];
+    assert_eq!(
+        Arc::as_ptr(&x.into_iter().topk(1).into_iter().next().unwrap()),
+        Arc::as_ptr(&chosen)
+    );
+}
+
+#[test]
+fn test_topk_by_empty() {
+    let x: Vec<usize> = Vec::new();
+    assert_eq!(
+        x.into_iter()
+            .topk_by(3, |x| x + 1)
+            .into_iter()
+            .collect_vec(),
+        Vec::new()
+    );
+}
+
+#[test]
+fn test_topk_by_take_zero() {
+    let x: Vec<usize> = vec![3];
+    assert_eq!(
+        x.into_iter()
+            .topk_by(0, |x| x * 2)
+            .into_iter()
+            .collect_vec(),
+        Vec::new()
+    );
+}
+
+#[test]
+fn test_topk_by_take_more() {
+    let x: Vec<usize> = vec![0, 1, 2];
+    assert_eq!(
+        x.into_iter().topk_by(5, |_| 0).into_iter().collect_vec(),
+        vec![0, 1, 2]
+    );
+}
+
+#[test_case(0)]
+#[test_case(1)]
+#[test_case(2)]
+fn test_topk_by_reverse_shuffled(seed: u64) {
+    let mut x: Vec<usize> = vec![0, 1, 1, 2, 3, 4, 8, 9, 9, 10];
+    x.shuffle(&mut StdRng::seed_from_u64(seed));
+    assert_eq!(
+        x.into_iter()
+            .topk_by(3, |x| Reverse(*x))
+            .into_iter()
+            .collect_vec(),
+        vec![1, 1, 0]
+    );
+}
+
+#[test]
+fn test_topk_by_parity() {
+    let x: Vec<usize> = vec![0, 1, 1, 2, 3, 4, 8, 9, 9, 10];
+    assert_eq!(
+        x.into_iter()
+            .topk_by(4, |x| x % 2)
+            .into_iter()
+            .collect_vec(),
+        vec![1, 1, 3, 9]
+    );
+}
+
+#[test]
+fn test_topk_by_parity_smaller() {
+    let x: Vec<usize> = vec![0, 1, 1, 2, 3, 4, 8, 9, 9, 10];
+    let length = x.len();
+    let mut count = 0;
+    assert_eq!(
+        x.into_iter()
+            .topk_by(3, |x| {
+                count += 1;
+                x % 2
+            })
+            .into_iter()
+            .collect_vec(),
+        vec![1, 1, 3]
+    );
+    assert_eq!(count, length);
+}
+
+#[test]
+fn test_topk_by_fn_pointers() {
+    let chosen = Arc::new(0);
+    let x = vec![Arc::clone(&chosen), Arc::new(0)];
+    assert_eq!(
+        Arc::as_ptr(
+            &x.into_iter()
+                .topk_by(1, |x| **x)
+                .into_iter()
+                .next()
+                .unwrap()
+        ),
+        Arc::as_ptr(&chosen)
+    );
+}
+
+#[test]
+fn test_topk_by_fn_pointers_multiple() {
+    let first = Arc::new(0);
+    let second = Arc::new(0);
+    let x = vec![Arc::clone(&first), Arc::clone(&second)];
+    let topk = &mut x.into_iter().topk_by(2, |x| **x).into_iter();
+    assert_eq!(Arc::as_ptr(&topk.next().unwrap()), Arc::as_ptr(&first));
+    assert_eq!(Arc::as_ptr(&topk.next().unwrap()), Arc::as_ptr(&second));
 }
