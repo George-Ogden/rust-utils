e02e6ff85df6d40de70955f0a629ecc1671ade5b
George Ogden <george-ogden@outlook.com>
2026-06-15 21:10:55 +0100

    Define pair trait with first and second

---
+++ src/pair.rs
@@ -0,0 +1,35 @@
+pub trait Pair<T, U> {
+    #[must_use]
+    fn first(self) -> T;
+
+    #[must_use]
+    fn second(self) -> U;
+}
+
+impl<T, U> Pair<T, U> for (T, U) {
+    #[inline]
+    fn first(self) -> T {
+        self.0
+    }
+
+    #[inline]
+    fn second(self) -> U {
+        self.1
+    }
+}
+
+impl<'a, T, U> Pair<&'a T, &'a U> for &'a (T, U) {
+    #[inline]
+    fn first(self) -> &'a T {
+        &self.0
+    }
+
+    #[inline]
+    fn second(self) -> &'a U {
+        &self.1
+    }
+}
+
+#[cfg(test)]
+#[path = "pair_test.rs"]
+mod test;
---
+++ src/pair_test.rs
@@ -0,0 +1,18 @@
+use super::*;
+use pretty_assertions::assert_eq;
+
+#[test]
+#[expect(clippy::float_cmp)]
+fn test_tuple_pair() {
+    let pair = (3, 5.0);
+    assert_eq!(pair.first(), 3);
+    assert_eq!(pair.second(), 5.0);
+}
+
+#[test]
+fn test_tuple_ref_pair() {
+    let tuple = (true, "right");
+    let pair = &tuple;
+    assert_eq!(pair.first(), &true);
+    assert_eq!(pair.second(), &"right");
+}
