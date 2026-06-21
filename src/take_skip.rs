4a94863207f951b5dfbdce95bfaea335d2b18ea9
George Ogden <george-ogden@outlook.com>
2026-06-21 13:48:13 +0100

    Add documentation to the take-skip method

--- src/take_skip.rs
+++ src/take_skip.rs
@@ -1,5 +1,11 @@
 pub trait TakeSkip: Iterator + Sized {
     #[inline]
+    /// Split an iterator at a given index.
+    ///
+    /// This eagerly collects the first `n` elements as a vector
+    /// and returns the original iterator with the remaining elements.
+    ///
+    /// If the iterator contains fewer than `n` elements, fewer than `n` are collected.
     fn take_skip(mut self, n: usize) -> (Vec<Self::Item>, Self) {
         let mut take = Vec::with_capacity(n);
         for _ in 0..n {
